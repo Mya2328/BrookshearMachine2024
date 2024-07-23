73462e7f66773178bb2f8addc0af63f8a3f58e73
Mya2328 <myathuzarlwin01@gmail.com>
2024-07-23 13:39:54 +0100

    23 July changes in UI

---
+++ vm/vmapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('api/upload_file/', views.upload_file, name='upload_file'),
+    path('api/vmonline4/step/', views.call_vmonline4_step, name='call_vmonline4_step'),
+    path('api/vmonline4/reset/', views.call_vmonline4_reset, name='call_vmonline4_reset'),
+    path('api/vmonline4/run/', views.call_vmonline4_run, name='call_vmonline4_run'),
+    path('api/vmonline4/load/', views.call_vmonline4_load, name='call_vmonline4_load'),
+    path('api/vmonline4/update_memory/', views.call_vmonline4_update_memory, name='call_vmonline4_update_memory'),
+]
