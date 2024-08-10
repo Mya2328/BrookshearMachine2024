8dad904f91fdb5a7000a411094f8ad1a4e33057b
Mya2328 <myathuzarlwin01@gmail.com>
2024-08-10 08:39:30 +0100

    Update(10th Aug)

--- vm/input.asm
+++ vm/input.asm
@@ -1,17 +1,49 @@
-NOP
-LOADI R1, 3
-LOADI R2, 4
-ADD R3, R1, R2
-STORE R3, 31
-LOADI R3, 1
-LOADI R4, 8
-ADD R5, R3, R4
-OR R1, R2, R3
-STORE R1, 32
-AND R4, R5, R6
-STORE R4, 33
-XOR R4, R5, R6
-STORE R4, 34
-SHR R4, 2 
-STORE R4, 35
+; Clear display memory (0x80 to 0xFF)
+LOADI R1, 80    ; Start address of display memory
+LOADI R2, 0     ; Value to clear
+
+CLEAR_LOOP:
+STORE R2, R1    ; Clear memory at R1
+ADD R1, R1, 1   ; Increment address
+LOADI R3, FF    ; Load upper bound address
+CMP R1, R3      ; Compare current address with upper bound
+JLT CLEAR_LOOP  ; Jump to CLEAR_LOOP if R1 < FF
+
+; Draw letter 'A' in a 10x10 grid
+; Top horizontal line
+LOADI R1, 88    ; Address 0x88
+LOADI R2, 7E    ; 01111110 (top line of 'A')
+STORE R2, R1    ; Store the line
+
+; Next line
+LOADI R1, 8A    ; Address 0x8A
+LOADI R2, 81    ; 10000001
+STORE R2, R1    ; Store the line
+
+; Next line
+LOADI R1, 8C    ; Address 0x8C
+LOADI R2, 81    ; 10000001
+STORE R2, R1    ; Store the line
+
+; Next line (middle horizontal line)
+LOADI R1, 8E    ; Address 0x8E
+LOADI R2, FF    ; 11111111
+STORE R2, R1    ; Store the line
+
+; Next line
+LOADI R1, 90    ; Address 0x90
+LOADI R2, 81    ; 10000001
+STORE R2, R1    ; Store the line
+
+; Next line
+LOADI R1, 92    ; Address 0x92
+LOADI R2, 81    ; 10000001
+STORE R2, R1    ; Store the line
+
+; Next line
+LOADI R1, 94    ; Address 0x94
+LOADI R2, 81    ; 10000001
+STORE R2, R1    ; Store the line
+
+; Halt the program
 HALT
---
+++ vm/input_c26dm2m.asm
@@ -0,0 +1,31 @@
+    LOADI R0, 0x80  ; Start address in memory (0x80)
+
+    ; Row 1: 0111110 -> 0x3E
+    LOADI R1, 0x3E
+    STORE R1, 0x80  ; Store at memory address 0x80
+
+    ; Row 2: 1000001 -> 0x41
+    LOADI R1, 0x41
+    STORE R1, 0x81  ; Store at memory address 0x81
+
+    ; Row 3: 1000001 -> 0x41
+    LOADI R1, 0x41
+    STORE R1, 0x82  ; Store at memory address 0x82
+
+    ; Row 4: 1111111 -> 0x7F
+    LOADI R1, 0x7F
+    STORE R1, 0x83  ; Store at memory address 0x83
+
+    ; Row 5: 1000001 -> 0x41
+    LOADI R1, 0x41
+    STORE R1, 0x84  ; Store at memory address 0x84
+
+    ; Row 6: 1000001 -> 0x41
+    LOADI R1, 0x41
+    STORE R1, 0x85  ; Store at memory address 0x85
+
+    ; Row 7: 1000001 -> 0x41
+    LOADI R1, 0x41
+    STORE R1, 0x86  ; Store at memory address 0x86
+
+    NOP             ; End of program
